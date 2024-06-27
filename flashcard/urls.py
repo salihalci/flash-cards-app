c366f9942946b7ae1cd58125d0e14c3153f2c648
salci <salih.alci@gmail.com>
2024-06-27 12:35:12 +0300

    copied base files -not finished initial config

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cardapp import views
 
 urlpatterns = [
+        # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+
+    # Todos
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
